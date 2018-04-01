d0419324b9a7e2f65ff8231cd89b3c381853addd
foundry27 <xfoundry27@yahoo.ca>
2018-04-01 13:39:00 -0400

    Split out and modernize CMakeLists.txt into individual build configurations for includes, tests, and third party sources

--- test/test_bus_builder.cpp
+++ test/test_bus_builder.cpp
@@ -19,16 +19,20 @@ int main() {
     constexpr auto bus_template = veho::make_bus_template<sam3x::rev_8e>()
             .with_transmitters<7>()
             .record_timestamps()
-            .on(fm::exact<20>(), [](const custom_thing_1& t1, const veho::frame<sam3x::rev_8e>& f){
+            .on(fm::exact<20>(),
+                    [](const custom_thing_1& t1, const veho::frame<sam3x::rev_8e>& f){
                 std::printf("asdas");
             })
-            .on(fm::exact<24>(), [](const custom_thing_2& t2, const veho::frame<sam3x::rev_8e>& f){
+            .on(fm::exact<24>(),
+                    [](const custom_thing_2& t2, const veho::frame<sam3x::rev_8e>& f){
                 std::abort();
             })
-            .on(fm::range<0, 4>(), [](const custom_thing_1& t1, const custom_thing_3& t3, const veho::frame<sam3x::rev_8e>& f){
+            .on(fm::range<0, 4>(),
+                    [](const custom_thing_1& t1, const custom_thing_3& t3, const veho::frame<sam3x::rev_8e>& f){
                 std::exit(0);
             })
-            .on(fm::range<6, 10>(), [](const custom_thing_3& t3, const veho::frame<sam3x::rev_8e>& f){
+            .on(fm::range<6, 10>(),
+                    [](const custom_thing_3& t3, const veho::frame<sam3x::rev_8e>& f){
                 std::puts("foo");
             })
             .build();
